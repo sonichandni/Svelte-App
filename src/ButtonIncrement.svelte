987511a02773d7252e9151145934557d896a5109
Chandni Soni <soni.chandni.415@gmail.com>
2024-01-03 14:20:46 +0530

    run arbitrary statements reactively

--- src/ButtonIncrement.svelte
+++ src/ButtonIncrement.svelte
@@ -4,6 +4,13 @@
     let increment = () => count++;
 
     $: doubled = count * 2;
+    
+    $: if (count >= 10) {
+		alert('count is dangerously high!');
+		count = 0;
+	}
+
+
 </script>
 
 <button on:click={increment}>Clicked {count} {count === 1 ? 'time' : 'times'}</button>
