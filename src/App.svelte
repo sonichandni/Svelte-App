774e6b85739030c44ee66600fd5e08ec11807bf2
Chandni Soni <soni.chandni.415@gmail.com>
2024-01-03 16:03:07 +0530

    array push

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Price from './Price.svelte';
 	import ButtonIncrement from './ButtonIncrement.svelte';
+    import Array from './Array.svelte';
 </script>
 
 <p>Styled!</p>
 <Price />
 <ButtonIncrement />
+<Array />
 <style>
 	p {
 		color: purple;
