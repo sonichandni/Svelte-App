594b3c78d2be4e5debfa710c8d5243cb260d873e
Chandni Soni <soni.chandni.415@gmail.com>
2024-01-03 12:15:22 +0530

    declarations

--- src/ButtonIncrement.svelte
+++ src/ButtonIncrement.svelte
@@ -2,6 +2,9 @@
     let count = 0;
 
     let increment = () => count++;
+
+    $: doubled = count * 2;
 </script>
 
 <button on:click={increment}>Clicked {count} {count === 1 ? 'time' : 'times'}</button>
+<p> Double of {count} is {doubled}</p>
